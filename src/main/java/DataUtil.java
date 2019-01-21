d48a2b920730c876a888ef6ad98ce05f23ddb775
Oleksandr Shendrikov <oshen@softserveinc.com>
2019-01-21 14:56:03 +0200

    Created DataUtil class

---
+++ src/main/java/DataUtil.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataUtil {
+
+    public static void getNamesList() {
+        List<String> names = new ArrayList<String>();
+        names.add("Anna");
+        names.add("Peter");
+        names.add("Alex");
+        names.add("Tanya");
+    }
+}
